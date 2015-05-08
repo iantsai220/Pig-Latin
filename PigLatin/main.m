9c1bee3418189ccbc74793f8e8da62478cc805e8
iantsai <iantsai86@gmail.com>
2015-05-08 15:52:10 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Ian Tsai on 2015-05-08.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
