ced92c7a3b65b9963075d9b1f9a569f36c6a84b3
iantsai <iantsai86@gmail.com>
2015-05-10 12:38:36 -0700

    first commit

---
+++ PigLatin/NSString+PigLatin.h
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatin.h
+//  PigLatin
+//
+//  Created by Ian Tsai on 2015-05-08.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PigLatin)
+
+@end
---
+++ PigLatin/NSString+PigLatin.m
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatin.m
+//  PigLatin
+//
+//  Created by Ian Tsai on 2015-05-08.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import "NSString+PigLatin.h"
+
+@implementation NSString (PigLatin)
+
+@end
